19dbb5ac4ae2232c6b44a012b249e5dd7be8c191
Dat Le <thanhdat952006@gmail.com>
2026-03-05 21:45:41 +0700

    Updated HTML structure to display data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+import getWeatherData from "./logic/weather";
+import { renderWeatherData, renderError } from "./ui/display-info";
+
+const cityInput = document.getElementById("city-input");
+const searchBtn = document.getElementById("search-btn");
+
+searchBtn.addEventListener("click", async () => {
+    try{
+        const data = await getWeatherData(cityInput.value);
+        console.log(data);
+        renderWeatherData(data);
+    }
+    catch(error){
+        renderError(error);
+    }
+});
